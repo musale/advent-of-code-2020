d7a8d7ab83eb11ffcaca6db39c981d702fdd033e
Martin Musale <martinmshale@gmail.com>
2020-12-01 10:51:53 +0300

    Add the solution for python day 01 part 2

--- day_01/py3/main.py
+++ day_01/py3/main.py
@@ -8,6 +8,15 @@ def part_one(arr: Sequence[int]) -> int:
             return n * target
 
 
+def part_two(arr: Sequence[int]) -> int:
+    for i in arr:
+        for j in arr[1:]:
+            target = 2020 - i - j
+            if target in arr[2:]:
+                return i * j*target
+
+
 if __name__ == "__main__":
     nums = [int(n) for n in open("day_01/input.txt").read().split("\n")]
     print(part_one(nums))
+    print(part_two(nums))
