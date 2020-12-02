16841755ece57419aff5713f4b436ac77ac3defc
Martin Musale <martinmshale@gmail.com>
2020-12-02 13:54:19 +0300

    Add the solution for golang day 02 part II

--- day_02/go/main.go
+++ day_02/go/main.go
@@ -11,19 +11,38 @@ import (
 
 func main() {
 	policies := loadPasswordsAndPolicies()
-	validPasswords := 0
+	var validCounts, validPasswords int
+
 	for _, policy := range policies {
 		if policy.WithinMinMax() {
 			validPasswords++
 		}
+		if policy.WithinPos() {
+			validCounts++
+		}
 	}
 	fmt.Println(validPasswords)
+	fmt.Println(validCounts)
 }
 
 // Password represents the password and the policy
 type Password struct {
 	min, max            int
 	character, password string
+}
+
+// WithinPos checks whether a password character is within the
+// index positions that are specified
+func (p *Password) WithinPos() bool {
+	hasCharAtPos := string(p.password[p.min-1]) == p.character
+	lacksCharAtPos := string(p.password[p.max-1]) != p.character
+	reverseHasCharAtPos := string(p.password[p.min-1]) != p.character
+	reverseLacksCharAtPos := string(p.password[p.max-1]) == p.character
+
+	if hasCharAtPos && lacksCharAtPos || reverseHasCharAtPos && reverseLacksCharAtPos {
+		return true
+	}
+	return false
 }
 
 // WithinMinMax checks if a password is within the min/max requirements
