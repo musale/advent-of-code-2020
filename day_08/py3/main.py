ddd3218b78628622a505b9965c1ede92defdaf80
Martin Musale <martinmshale@gmail.com>
2020-12-14 12:17:29 +0300

    Add the code for day 08 part 2

--- day_08/py3/main.py
+++ day_08/py3/main.py
@@ -1,41 +1,12 @@
+from typing import Tuple
 
 
-class IntCode:
-    def __init__(self):
-        self.accumulator = 0
-        self.visited = set()
-
-    def jmp(self, command) -> int:
-        operator, value = command[:1], int(command[1:])
-        if ("jmp", command) in self.visited:
-            print(f"Infinity loop detected at jmp {self.accumulator}")
-            return self.accumulator
-        if operator == "-":
-            value = value * -1
-        self.accumulator += value
-        self.visited.add(("jmp", command))
-        return self.accumulator
-
-    def acc(self, command):
-        operator, value = command[:1], int(command[1:])
-        if ("acc", command) in self.visited:
-            print(f"Infinity loop detected at acc {self.accumulator}")
-            return self.accumulator
-        if operator == "-":
-            value = value * -1
-        self.accumulator += value
-        self.visited.add(("acc", command))
-        return self.accumulator
-
-
-if __name__ == "__main__":
-    with open("day_08/input.txt") as f:
-        commands = [line.strip() for line in f.readlines()]
-
+def part_1(commands) -> Tuple[int, bool]:
     idx = 0
     command = commands[idx]
     visited = set()
     accumulator = 0
+    has_loop = False
     while idx not in visited:
         visited.add(idx)
         instruction, op, value = command[:3], command[4:5], int(command[5:])
@@ -50,5 +21,31 @@ if __name__ == "__main__":
             idx += 1
         elif instruction == "jmp":
             idx += value
-        command = commands[idx]
-    print(f"Part 1: {accumulator}")
+        try:
+            command = commands[idx]
+            if idx in visited:
+                has_loop = True
+        except IndexError as err:
+            break
+    return accumulator, has_loop
+
+
+def part_2(commands) -> int:
+    accumulator = 0
+    swapper = {"nop": "jmp", "jmp": "nop"}
+    for idx, command in enumerate(commands):
+        instruction = command[:3]
+        if instruction in ["jmp", "nop"]:
+            swapped = command.replace(instruction, swapper[instruction])
+            accumulator, has_loop = part_1(
+                commands[:idx]+[swapped]+commands[idx+1:])
+            if not has_loop:
+                return accumulator
+    return accumulator
+
+
+if __name__ == "__main__":
+    with open("day_08/input.txt") as f:
+        commands = [line.strip() for line in f.readlines()]
+        print(f"Part 1: {part_1(commands)}")
+        print(f"Part 2: {part_2(commands)}")
